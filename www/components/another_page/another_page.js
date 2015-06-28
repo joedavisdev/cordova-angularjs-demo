8afb678f8529f96b7675b0125db4bc82e9a29045
Joe Davis <joemdavis@gmail.com>
2015-06-28 13:15:27 +0100

    Feature: Added a list to another page

--- www/components/another_page/another_page.js
+++ www/components/another_page/another_page.js
@@ -6,14 +6,22 @@ function AnotherController($scope, $location) {
     $scope.go = function(path) {
         $location.path(path);
     };
-
+    $scope.listClick = function(item) {
+    
+    };
     /* init() - Immediate function to setup variables
     *  Personal preference. Improves code clarity as
     *  completixy increases
     * */
     self.init = (function() {
         // Scope setup
-        $scope.heading = "another page"
+        $scope.heading = "another page";
+        $scope.some_list = [
+        'interesting',
+        'stuff',
+        'goes',
+        'here'
+        ];
     })();
 }
 
